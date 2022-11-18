8140d7102909043d4289c7e31cd623d49d5a7afb
camposcostero22 <camposcostero22@gmail.com>
2022-11-18 11:24:13 -0400

    GiftExpertApp cambios

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -6,29 +6,26 @@ import { GifGrid } from './components/GifGrid';
 //PARA CREAR MAS RAPIDO UN COMPONENTE SE USA EL SNIPPET RAFCP
 export const GifExpertApp = () =>{ 
      //Se debe usar un hook
-    const [categories, setcategories] = useState(['One piece']);
+    const [categories, setcategories] = useState(['One piece','Bleach']);
 
-//   const handleAdd = ()=>{
-//       setcategories(['Gladys',...categories]);
-//       setcategories(cats => [...cats,'Gladys']);
-//   }
+    const handleAdd = ()=>{
+        setcategories(['Dragon ball',...categories]);
+        //setcategories(cats => [...cats,'Dragon ball']);
+    }
     return (
-        <>
-            <h2>GifExpertApp</h2>
-            <AddCategory setcategories={setcategories}/>
-            <hr/>
-
-            <ol>
-                {
-                    categories.map(category => (
-                      <GifGrid
-                        key={category} 
-                        category={category}
-                      />
-                            ))
-                }
-            </ol>
-        </>
+      <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setcategories={setcategories}/>
+        <hr/>
+        <button onClick={handleAdd}>Agregar</button>
+        <ol>
+            {
+           categories.map(category => (
+                  <GifGrid key={category} category={category}/>
+                    ))
+            }
+        </ol>
+      </>
     );
         
 };
